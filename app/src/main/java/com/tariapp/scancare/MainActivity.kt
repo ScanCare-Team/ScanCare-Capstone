e9a63b9317851200a4447ef1a6913291670d835d
andibatari <andibatari2311@gmail.com>
2024-11-15 22:35:35 +0800

    first commit

---
+++ app/src/main/java/com/tariapp/scancare/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.tariapp.scancare
+
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
+import com.tariapp.scancare.databinding.ActivityMainBinding
+
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        setSupportActionBar(binding.appBar.toolbar)
+//
+        val bottomAppBar: BottomAppBar = binding.botomApp
+        val fab : FloatingActionButton = binding.fabScan
+
+
+        // Pastikan FAB terhubung dengan BottomAppBar
+        bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+        fab.setOnClickListener {
+            Toast.makeText(this, "FAB Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_profile
+            )
+        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
